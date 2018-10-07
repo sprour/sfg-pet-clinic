15b43d40da71237375c57425562813822fe133dc
Sergei Prohhorov <sprohorov@leongaming.com>
2018-10-07 12:38:17 +0300

    Create a Multi module project #3

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -2,18 +2,16 @@ package guru.springframework.sfgpetclinic.model;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
-import javax.validation.constraints.NotEmpty;
-
 
 @MappedSuperclass
 public class Person {
 
     @Column(name="first_name")
-    @NotEmpty
+
     private String firstName;
 
     @Column(name = "last_name")
-    @NotEmpty
+
     private String lastName;
 
     public String getFirstName() {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,13 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-@Entity
-@Table(name = "pets")
+
 public class Pet {
 
     private LocalDate birthDate;
